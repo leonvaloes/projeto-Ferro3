4424a618f335cb8e8341174a82d8f82c5c3b1ab8
Leonardo Ribeiro <leonardovaloes@hotmail.com>
2024-05-28 22:25:05 -0300

    primeiro commit

---
+++ AtivoOperante2024/src/main/java/br/unoeste/jinwoo/ativooperante2024/restcontrollers/CidadaoRestContoller.java
@@ -0,0 +1,16 @@
+package br.unoeste.jinwoo.ativooperante2024.restcontrollers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "apis/cidadadao/")
+public class CidadaoRestContoller {
+    @GetMapping(value = "teste-conexao")
+    public String testeConexao()
+    {
+        return "conectado";
+    }
+    //demais apis
+}
---
+++ readme.md
@@ -0,0 +1,6 @@
+`` docker-compose up -d``
+``docker-compose ps -q db``
+``vamos chamar esse alor gedado de id``
+``docker cp ativooperante.sql {id}``
+``docker exec -it{id}``
+``psql -U postgres -d Ativo-Operante -f /ativooperante.sql``
